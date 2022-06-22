43af2cc21a21db683669a7b6a6747bdfabf60f61
vipinable <vipinable@gmail.com>
2022-06-22 12:31:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,36 @@ resource "aws_iam_role_policy_attachment" "AmazonManaged-AmazonEKSVPCResourceCon
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.eks-cluster-role.name
 }
+  
+resource "aws_iam_role" "eks-node_role" {
+  name = "eks-node_role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonManaged-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonManaged-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonManaged-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_role.name
+}
  
 resource "aws_cloudwatch_log_group" "cluster_log" {
   name              = "/aws/eks/${var.cluster_name}/cluster"
